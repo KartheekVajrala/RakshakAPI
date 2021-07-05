d40d0eabc271f82faaf04cc480da10d660ae7ba0
KartheekVajrala <kartheekvajrala@gmail.com>
2021-07-05 21:35:36 +0530

    third api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const fs = require('fs');
 var session = require('express-session');
 var uid = require('rand-token').uid;
 const app = express();
@@ -9,7 +10,13 @@ const app = express();
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(session({secret: 'secret-key'}));
+app.use(session({
+    secret : 'secret-key',
+    resave :false,
+    saveUninitialized: true,
+
+}));
+
 mongoose.connect("mongodb://localhost:27017/RakshakDB",{useNewUrlParser: true, useUnifiedTopology: true});
 
 const userSchema = new mongoose.Schema({
@@ -17,6 +24,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   role: String,
+  token:String
 });
 
 const User = mongoose.model("User",userSchema);
@@ -30,14 +38,20 @@ app.post("/campus/main/register",function(req,res){
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
-    role: req.body.role
+    role: req.body.role,
+    token:uid(16)
   });
   user.save();
   res.send({"message":"Registered successfully"});
 });
 
+app.get("/campus/main/logout",function(req,res){
+  req.session.destroy();
+  res.send({"message":"Successfully logged out"});
+});
+
 app.post("/campus/main/login",function(req,res){
-  User.findOne({email: req.body.username},function(err, foundUser){
+  User.findOne({username: req.body.username},function(err, foundUser){
     if(err){
       res.send(err);
     }else if(!foundUser){
@@ -45,8 +59,10 @@ app.post("/campus/main/login",function(req,res){
         "message": "Not Found"
       });
     }else if(foundUser.password === req.body.password){
+      req.session.sessionId = foundUser.token;
+      console.log(req.session);
       res.send({
-        "message":"sucessfully logined",
+        "message":"sucessfully logged in",
          "token":foundUser.token,
          "role":foundUser.role
       });
@@ -54,14 +70,29 @@ app.post("/campus/main/login",function(req,res){
   });
 });
 
-// app.get("/",function(req,res){
-//   res.sendFile(__dirname+"/index.html");
-// });
-//
-// app.post("/",function(req,res){
-//   console.log(req.body);
-//   res.redirect("/");
-// });
+app.get("/campus/main/dashboard",function(req,res){
+  if(!req.session.sessionId){
+    res.send("login first");
+  }else{
+    User.findOne({token:req.session.sessionId},function(err, foundUser){
+      if(!err && foundUser){
+        res.send("you are "+foundUser.username);
+      }else{
+        res.send("session not found.");
+      }
+    });
+  }
+});
+
+app.get("/campus/simulation/policyplanner",function(req,res){
+  if(!req.session.sessionId){
+    res.send("login first");
+  }else{
+    fs.readFile('defaultData.json',function(err,data){
+      res.send(JSON.parse(data));
+    });
+  }
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
