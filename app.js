85bba903d0e78078f07a8de3a442f5355177f26e
Aakarsh2000 <58129140+Aakarsh2000@users.noreply.github.com>
2021-07-23 16:42:11 +0530

    Added 16th API and campus object

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ const fs = require('fs');
 var session = require('express-session');
 var uid = require('rand-token').uid;
 const app = express();
+const cors = require('cors')
+
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200
+}
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
 app.use(session({
     secret : 'secret-key',
     resave :false,
@@ -26,10 +35,17 @@ const userSchema = new mongoose.Schema({
   role: String,
   token:String,
   savedParams: Array,
-  campusbuildings: Array
+});
+
+const campusSchema = new mongoose.Schema({
+  campusname : String,
+  buildings : Array,
+  classes : Array
 });
 
 const User = mongoose.model("User",userSchema);
+
+const Campus = mongoose.model("Campus",campusSchema);
 
 app.get("/campus/main/campusname",function(req,res){
   res.send({"campusname":["madras","delhi"]});
@@ -43,7 +59,6 @@ app.post("/campus/main/register",function(req,res){
     role: req.body.role,
     token: uid(16),
     savedParams: [],
-    campusbuildings: []
   });
   user.save();
   res.send({"message":"Registered successfully"});
@@ -55,6 +70,24 @@ app.get("/campus/main/logout",function(req,res){
 });
 
 app.post("/campus/main/login",function(req,res){
+  console.log(req.body);
+  Campus.findOne({campusname:req.body.campusname},function(err,foundCampus){
+    if(err){
+      console.log(err);
+    }else{
+      if(!foundCampus){
+        campus = new Campus({
+          campusname : req.body.campusname,
+          buildings : [],
+          classes : []
+        });
+        campus.save();
+        console.log("campus saved");
+      }else{
+        console.log("campus aldready exists");
+      }
+    }
+  });
   User.findOne({username: req.body.username},function(err, foundUser){
     if(err){
       res.send(err);
@@ -64,6 +97,7 @@ app.post("/campus/main/login",function(req,res){
       });
     }else if(foundUser.password === req.body.password){
       req.session.sessionId = foundUser.token;
+      req.session.campusname = req.body.campusname;
       console.log(req.session);
       res.send({
         "message":"sucessfully logged in",
@@ -153,23 +187,40 @@ app.post("/campus/masterdata/campusbuildings/addbuilding", bodyParser.json() ,fu
   if(!req.session.sessionId){
     res.send("login first");
   }else{
-    User.findOne({token: req.session.sessionId},function(err,foundUser){
+    Campus.findOne({campusname:req.session.campusname},function(err,foundCampus){
       if(err){
         res.send(err);
       }else{
-        if(foundUser){
-          if(foundUser.campusbuildings.length === 10){
+        if(foundCampus){
+          if(foundCampus.buildings.length === 10){
             res.send({"message":"limit reached"});
           }else{
-            User.findOneAndUpdate({token: req.session.sessionId},{$push : {campusbuildings : req.body}},function(err,success){
-              if(err){
-                  res.send(err);
-                }else{
-                  res.send({"Message": "Saved Successfully"});
-                }
-            });
+            foundCampus.buildings.push(req.body);
+            foundCampus.save();
+            res.send({"message":"Saved Successfully"});
           }
         }
+      }
+    });
+  }
+});
+//16th API
+app.delete('/campus/masterdata/campusbuildings/deletebuilding',function(req,res){
+  if(!req.session.sessionId){
+    console.log("login first");
+  }else{
+    Campus.findOne({campusname:req.session.campusname},function(err,foundCampus){
+      if(!err && foundCampus){
+        for(let i=0;i<foundCampus.buildings.length;i++){
+          if(foundCampus.buildings[i].BuildingId === req.body.BuildingId){
+            foundCampus.buildings.splice(i,1);
+            break;
+          }
+        }
+        foundCampus.save(error => {if(error){console.log(error);}});
+        res.send({"message":"Building deleted successfully"});
+      }else{
+        res.send({"message":"Campus not found"});
       }
     });
   }
