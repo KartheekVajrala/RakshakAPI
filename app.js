396312674627614755d9185c309070f4f3520ec4
Aakarsh2000 <58129140+Aakarsh2000@users.noreply.github.com>
2021-07-25 21:49:46 +0530

    Added 47rd-49th API

--- app.js
+++ app.js
@@ -780,48 +780,72 @@ app.delete('/campus/masterdata/facultydetails/delete',function(req,res){
     });
   }
 });
-//47th API dummy
+//47th API
 app.get("/campus/masterdata/staffdetails", function (req, res) {
 	if (!req.session.sessionId) {
 		res.send("login first");
 	} else {
-    res.send(
-        [
-         {
-          'ID':1234,
-          'StaffCategory ':1,
-          'WorkplaceBuildingName':'AdminBuildingEast',
-          "ResidenceBuildingName":'Type_B_Block1',
-          "AdultFamilyMembers":5,
-          "NoofChildren":3
-         },
-         {
-          'ID':1234,
-          'StaffCategory ':1,
-          'WorkplaceBuildingName':'AdminBuildingWest',
-          "ResidenceBuildingName":'Type_A_Block1',
-          "AdultFamilyMembers":4,
-          "NoofChildren":2
-         }
-        ]
-     );
+    Campus.findOne({campusname:req.session.campusname},function(err,foundCampus){
+      if(!err && foundCampus){
+        let arr_class = []
+        for(let i = 0;i<foundCampus.staff_details.length;i++){
+          arr_class.push({"ID":foundCampus.staff_details[i].ID,
+          "StaffCategory":foundCampus.staff_details[i].StaffCategory,
+          "WorkplaceBuildingName":foundCampus.staff_details[i].WorkplaceBuildingName,
+          "ResidenceBuildingName":foundCampus.staff_details[i].ResidenceBuildingName,
+          "AdultFamilyMembers":foundCampus.staff_details[i].AdultFamilyMembers,
+          "NoofChildren":foundCampus.staff_details[i].NoofChildren,
+          "Status":"Enabled Or Disabled"}
+          );
+        }
+        res.send({"message":arr_class});
+      }else{
+        res.send({"message":"campus not found."});
+      }
+    });
   }
 });
-//48th API dummy
+//48th API
 app.post("/campus/masterdata/staffdetails/add", function (req, res) {
 	if (!req.session.sessionId) {
 		res.send("login first");
 	} else {
-    res.send({"Message":"Saved"});
+    Campus.findOne({campusname:req.session.campusname},function(err,foundCampus){
+      if(err){
+        res.send(err);
+      }else{
+        if(foundCampus){
+          if(foundCampus.staff_details.length === 10){
+            res.send({"message":"limit reached"});
+          }else{
+            foundCampus.staff_details.push(req.body);
+            foundCampus.save();
+            res.send({"Message":"Added"});
+          }
+        }
+      }
+    });
   }
 });
-//49th API dummy
+//49th API
 app.delete('/campus/masterdata/staffdetails/delete',function(req,res){
   if(!req.session.sessionId){
     console.log("login first");
   }else{
-    // code to delete
-    res.send({'message':'Deleted Successfully'})
+    Campus.findOne({campusname:req.session.campusname},function(err,foundCampus){
+      if(!err && foundCampus){
+        for(let i=0;i<foundCampus.staff_details.length;i++){
+          if(foundCampus.staff_details[i].ID === req.body.ID){
+            foundCampus.staff_details.splice(i,1);
+            break;
+          }
+        }
+        foundCampus.save(error => {if(error){console.log(error);}});
+        res.send({"message":"Deleted Successfully"});
+      }else{
+        res.send({"message":"Campus not found"});
+      }
+    });
   }
 });
 app.listen(3000, function() {
